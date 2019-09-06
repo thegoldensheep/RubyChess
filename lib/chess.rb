453ebc25fc90269781386b0b348ae816688bb7a0
Dillon Shepherd <dillonshepherd1991@gmail.com>
2019-09-05 20:00:47 -0400

    Added classes to chess.rb

---
+++ lib/chess.rb
@@ -0,0 +1,65 @@
+class Game
+  def initialize
+    
+  end
+end
+
+class Board
+  def initialize
+    
+  end
+end
+
+class Square
+  def initialize
+    
+  end
+end
+
+class Player
+  def initialize
+    
+  end
+end
+
+class GamePiece
+  def initialize
+    
+  end
+end
+
+class Pawn < GamePiece
+  def initialize
+    
+  end
+end
+
+class Rook < GamePiece
+  def initialize
+    
+  end
+end
+
+class Knight < GamePiece
+  def initialize
+    
+  end
+end
+
+class Bishop < GamePiece
+  def initialize
+    
+  end
+end
+
+class King < GamePiece
+  def initialize
+    
+  end
+end
+
+class Queen < GamePiece
+  def initialize
+    
+  end
+end
